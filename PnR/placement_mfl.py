c7ce971c439c3c6b8da631cec7b802c8c67c81de
Marty <harnesser+marty@gmail.com>
2007-10-11 20:00:30 +0000

    Initial checkin - before move.  wxDebuggy is currently broken

---
+++ PnR/placement_mfl.py
@@ -0,0 +1,47 @@
+# placement_mfl.py 
+#
+# Placement of instanciations in the current module view based on the minimisation of flight lines.
+#
+
+
+def columnize( driver_dict, inst, col_dict, load = []):
+    """ Find the drivers of the current inst, and set their
+    column numbers to one less than the current.
+
+    Look out for loops by doing something magical..."""
+
+    col_num = col_dict[inst] + 1
+    load.append(inst)
+
+    print "::", inst, driver_dict.keys()
+
+    #  Go through the drivers of this sink and update their
+    # column numbers if necessary
+    for driver in driver_dict[inst]:
+
+        # Loop dectection...
+        if driver in load :
+            print "Loop!!: ", driver, ":", load
+            continue
+
+        # Only update the column count if needed.  If the load
+        # is already to the right of this inst, then leave its
+        # col number alone. 
+        if col_num > col_dict[driver]:
+            col_dict[driver] = col_num
+            col_dict = columnize( driver_dict, driver, col_dict, load )
+
+    load.pop()
+    
+    if True:
+        for key in col_dict.keys():
+            print ("        " * ( col_dict[key] )) + key.center(8) 
+        print "-" * 80
+        print col_dict
+
+    return col_dict
+
+
+
+
+
