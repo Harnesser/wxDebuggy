d5f7e2435bf632ab27d65da8019b5b0e4389ddcb
Marty <harnesser+marty@gmail.com>
2007-11-22 00:12:09 +0000

    initial checkin

--- GUI/PnR/placement_mfl.py
+++ GUI/PnR/placement_mfl.py
@@ -8,6 +8,11 @@ def columnize( driver_dict, inst, col_dict, load = [], debug = True ):
     """ Find the drivers of the current inst, and set their
     column numbers to one less than the current.
 
+    col_dict[<instn_name>] = <column_number>
+
+    Column[0] = Input ports
+    Column[-1] = Output ports
+
     Look out for loops by doing something magical..."""
 
     col_num = col_dict[inst] + 1
@@ -43,17 +48,36 @@ def columnize( driver_dict, inst, col_dict, load = [], debug = True ):
 
 
 
-def place_instanciations( module, driver_dict, col_dict ):
-    """ Place the instanciations of the current module by minimizing the total length of 
-    all the connection flight lines.
+def yplacement( inst_col_dict, drawing_object_dict, driver_dict ):
+    """ Place the instanciations of the current module in the y-axis.
 
     At the minute, this is pure combinatorial.  Will take ages for big ccts. 
     """
 
-    for col in col_dict.values():
+    # Find the number of columns in this design
+    num_cols = max( inst_col_dict.values() ) + 1
 
-        for other_col in col_dict.values():
-            pass
+    # Loop thru the columns, sorting the instantiations in the y-axis as we proceed.
+    # Begin at column[2], sort column[1].  Group
+    # move to column[3] and sort column[2]
+    # Remember, in col_dict, [0] = input ports, and [-1] are output ports
+    # when placing the modules I'm only worrying about columns with actual modules
+    # in them.
+    for i in range(2,len(inst_col_dict)): # jump straight to the 2nd col of instns    
+
+        prev_col_drivers = {}
+        # Determine which connections go between col[i] and col[i-1]
+        for inst in inst_col_dict[i-1]: # driving modules
+                        
+            for driver_inst,driver_port in driver_dict.keys():
+                if driver_inst == inst:
+                     pass
+                        
+
+        for col in inst_col_dict.values():
+
+           for other_col in inst_col_dict.values():
+                pass
 
 
     
