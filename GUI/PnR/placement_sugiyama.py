5ce134d240029616c1c099da81ba9814bdebcc81
Marty <harnesser+marty@gmail.com>
2008-06-12 01:37:20 +0000

    Think I've got a decent first cut long edge breaker going

--- GUI/PnR/placement_sugiyama.py
+++ GUI/PnR/placement_sugiyama.py
@@ -9,17 +9,38 @@ Sugiyama's approach. This algorithm has a few stages:
  3: Replace any edges which cross more than one layer with dummy nodes
  4: Sort the nodes on each layer in a manner to reduce the crossover count
  
+This module swaps stages 1 & 2 - a recursive algorithm to produce the layering,
+then any arcs which are pointing backwards are reversed.
+
 """
 
 
 def build_graph(module):
     """ Build a graph for the current module to be displayed.
     """
+    #
+    # 1: Exract the graph from the circuit description    
+    graph_edges = extract_graph(module)
     
-    graph = extract_graph(module)
-    graph = remove_cycles(graph)
-    graph = determine_layering(graph)
-    graph = insert_dummy_nodes(graph)
+    #
+    # 2: Remove cycles
+    #graph = remove_cycles(graph)
+
+    #
+    # 3: Layer the graph
+    layer_dictionary = determine_layering(graph_edges)
+    
+    print "\nGraph Edges Dictionary"
+    for key in graph_edges.keys():
+        print key," :", graph_edges[key]
+        
+
+    #
+    # 4: Insert dummy nodes - this makes the graph 'proper'
+    graph = break_up_long_edges(layer_dictionary, graph_edges)
+    
+    #
+    # 5: Reduce crossovers by reordering the layers
     graph = reduce_crossings(graph)
     
     return graph
@@ -33,36 +54,124 @@ def extract_graph(module):
     
     driver_dictionary = build_driver_dictionary(module)
     connection_list = get_connection_list(driver_dictionary)
-    graph = get_graph_dictionary(connection_list)
+    graph_edges = get_graph_dictionary(connection_list)
     
+    return graph_edges
+    
+    
+def remove_cycles(graph, debug=True):
+    """ Remove cycles from graph.
+    Reverse certain edges to ensure the graph is acyclic.
+    """
+        
     
     return graph
     
     
-def remove_cycles(graph):
-    """ Remove cycles from graph.
-    Reverse certain edges to ensure the graph is acyclic.
-    """
-    
-    return acyclic_graph
-    
-    
-def determine_layering(graph):
+def determine_layering(graph, inst='_iport', col_dict = {}, path = [], debug = False ):
     """ Layer the graph.
-    """
     
-    return layered_graph
+    Find the drivers of the current inst, and set their
+    column numbers to one less than the current.
+
+    col_dict[<instn_name>] = <column_number>
+
+    Column[0] = Input ports
+    Column[-1] = Output ports
+
+    Look out for loops by doing something magical..."""
+
+    col_num = col_dict.get(inst, 0) + 1
+    path.append(inst)
+
+    print "::", inst, graph.keys()
+
+    #  Go through the drivers of this sink and update their
+    # column numbers if necessary
+    
+    for sink in graph.get(inst,[]):
+
+        print "SINK:" + sink
+        # Loop detection...
+        if sink in path :
+            print "Loop!!: ", sink, ":", path
+            continue
+
+        # Only update the column count if needed.  If the load
+        # is already to the right of this inst, then leave its
+        # col number alone. 
+        if col_num > col_dict.get(sink,0):
+            col_dict[sink] = col_num
+            col_dict = determine_layering( graph, sink, col_dict, path )
+            
+    path.pop()
+    
+    if debug:
+        for key in col_dict.keys():
+            print ("        " * ( col_dict[key] )) + key.center(8) 
+        print "-" * 80
+        print col_dict
+
+    return col_dict
     
     
-def insert_dummy_nodes(graph):
+    
+def break_up_long_edges(layer_dictionary, graph_edges, debug=True):
     """ Insert dummy nodes for long edges.
     Produces a 'proper graph'.    
     """
     
-    return proper_graph
+    # turn the layer graph inside out so that layer numbers are the keys.
+    graph_layers = {}
+    for key in layer_dictionary.keys():
+        graph_layers.setdefault( layer_dictionary[key], []).append(key)     
+
+    
+    # Dummy nodes are placed in long edges (span >1)
+    edges_for_removal = []
+    for u in graph_edges.keys():
+        for v in graph_edges[u]:
+            start_layer = layer_dictionary.get(v,0) 
+            end_layer   = layer_dictionary.get(u,0) 
+            span = abs( start_layer - end_layer )
+            
+            if span > 1: # we've found a long edge..
+                print "!Found a long edge: (%s,%s)" % (u,v)   
+                edges_for_removal.append( (u,v) )
+            
+    # Delete edges - can't delete items from lists when iterating over them
+    for u,v in edges_for_removal:
+        start_layer = layer_dictionary.get(v,0) 
+        end_layer   = layer_dictionary.get(u,0)        
+
+        start_vertice = u
+        for i in range( min(start_layer,end_layer) + 1, max(start_layer,end_layer) ):
+            new_vertice_name = '_' + u + '__to__' + v + '_' + str(i)
+            graph_layers[i].append(new_vertice_name)
+            graph_edges.setdefault(start_vertice,set()).add(new_vertice_name)
+                
+        graph_edges.setdefault(new_vertice_name,set()).add(v)
+                
+    #                
+    if debug:
+    
+        print "\nGraph Edges Dictionary"
+        for key in graph_edges.keys():
+            print key," :", graph_edges[key]        
+                    
+    
+        print "\nGraph Layers Dictionary"
+        for key in graph_layers.keys():
+            print key," :", graph_layers[key]  
+                    
+    return True
     
     
-def reduce_crossings(graph):
+    
+    
+    
+    
+def reduce_crossings(graph, debug=True):
     """ Reduce edge crossings.
     By reordering the nodes in each layer, the number of edge crossings is
     reduced.
@@ -200,7 +309,8 @@ def get_graph_dictionary(connection_list, debug=True):
              'E': ['F'],
              'F': ['C']}
 
-    Pins on each instantiation are ignored.
+    Pins on each instantiation are ignored.  Two additional vesrtices are added,
+    '_iport' which connects to input ports, and '_oport' which links output ports.
     
     See: http://www.python.org/doc/essays/graphs.html 
     """
@@ -208,18 +318,27 @@ def get_graph_dictionary(connection_list, debug=True):
     graph_dictionary = {}
     for source,sink in connection_list:
         
+        
         # Determine names for vertices
         source_name = source[0]
         if source_name.startswith('_'): # deal with ports
+            if source_name is '_iport': # '_iport' drives inputs
+                graph_dictionary.setdefault(source[0], []).append(source[1])
             source_name = source[1] 
     
         sink_name = sink[0]
         if sink_name.startswith('_'): # again, deal with ports
+            if sink_name is '_oport': # outputs drive '_oport'
+                graph_dictionary.setdefault(sink[1], []).append(sink[0]) 
             sink_name = sink[1] 
-    
+            
         # Now fill in the dictionary
         graph_dictionary.setdefault(source_name, []).append(sink_name)
         
+        
+    # remove duplicates
+    for key in graph_dictionary.keys():
+        graph_dictionary[key] = set( graph_dictionary[key] )
     
     if debug:
         print "\n\n### Graph Dictionary"
@@ -234,49 +353,6 @@ def get_graph_dictionary(connection_list, debug=True):
 ##
 ## ================================================================================
     
-    
-def columnize( driver_dict, inst, col_dict, load = [], debug = True ):
-    """ Find the drivers of the current inst, and set their
-    column numbers to one less than the current.
-
-    col_dict[<instn_name>] = <column_number>
-
-    Column[0] = Input ports
-    Column[-1] = Output ports
-
-    Look out for loops by doing something magical..."""
-
-    col_num = col_dict[inst] + 1
-    load.append(inst)
-
-    print "::", inst, driver_dict.keys()
-
-    #  Go through the drivers of this sink and update their
-    # column numbers if necessary
-    for driver in driver_dict[inst]:
-
-        # Loop dectection...
-        if driver in load :
-            print "Loop!!: ", driver, ":", load
-            continue
-
-        # Only update the column count if needed.  If the load
-        # is already to the right of this inst, then leave its
-        # col number alone. 
-        if col_num > col_dict[driver]:
-            col_dict[driver] = col_num
-            col_dict = columnize( driver_dict, driver, col_dict, load )
-
-    load.pop()
-    
-    if debug:
-        for key in col_dict.keys():
-            print ("        " * ( col_dict[key] )) + key.center(8) 
-        print "-" * 80
-        print col_dict
-
-    return col_dict
-
 
 
 def yplacement( inst_col_dict, drawing_object_dict, driver_dict ):
