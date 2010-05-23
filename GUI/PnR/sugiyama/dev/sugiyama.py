79d4246d13e05b8885378f4deac10223b0a8277b
Marty <harnesser+marty@gmail.com>
2010-05-23 06:35:46 +0000

    Up to section II.C in Sugiyama

--- GUI/PnR/sugiyama/dev/sugiyama.py
+++ GUI/PnR/sugiyama/dev/sugiyama.py
@@ -14,6 +14,7 @@ E = [  [('a','c'), ('a','d'), ('a','e'), ('a','f'), ('b','c'), ('b','f') ],
     
 levels  = len(V)
 
+
 ## -------------------------------------------------------------------------
 ##  Matrix Realisation of n-Level Hierarchies
 ## -------------------------------------------------------------------------
@@ -28,12 +29,39 @@ for i in xrange(levels-1):
         m[row][col] = 1
     
     M.append(m)
+ 
     
 for i in xrange(levels-1):
     print "M(%0d)" % (i)
     print M[i]
     print 
+       
+## -------------------------------------------------------------------------
+##  The Number of Crossings of n-Level Hierarchies
+## -------------------------------------------------------------------------
+def calc_k(i,j,k):
+    """ Calculate the crossings between an ordered pair of vertex rows. """
+    q = len( V[i+1] )
+    k_jk = 0
+    
+    for a in xrange(0,q-1):  # 0 ... q-2
+        for b in xrange(a+1, q): # a+1 ... q-1
+            k_jk += M[i][j][b] * M[i][k][a]
+    
+    return k_jk
 
-   
+def calc_layer_crossings(i):
+    p = len( V[i] )
+    K_M = 0
+    
+    for j in xrange(0,p-1):
+        for k in xrange(j+1,p):
+            K_M += calc_k(i, j, k )
+    
+    return K_M
+    
+for i in xrange(levels-1):
+    print "Layer", i, calc_layer_crossings(i)
+    
        
       
