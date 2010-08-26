cb9e9389534f984942d5c058cce118a745c5a896
Marty <harnesser+marty@gmail.com>
2010-08-26 03:27:18 +0100

    rejigged comments

--- wxDebuggy.py
+++ wxDebuggy.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
 """
-This is for git. Probably will abandon this branch.
-More changes, hopefully gitg will draw a nice picture.
+  wxDebuggy - An RTL Visualiser.
+  
+ * Reads in verilog files and displays schematics and hierarchy -DONE
+ * A splitter window, left = hier tree, right = autogen schematics -DONE
+ * Status bar shows current hierarchy path -DONE
+ * Window title shows name of module on view -DONE
+ * Schematic to be displayed is determined by tree selection. -DONE
+     Alternatively, double-clicking on the bounding box of a schematic's
+     child module will bring up the schematics of that module, and
+     update the hier tree selection accordingly. -DONE
+ * Doubleclicking the Right mouse button will move up the hierarchy -DONE
+ * Scrolling the mousewheel will zoom in on the cursor.
 """
-#  * Reads in verilog files and displays schematics and hierarchy -DONE
-#  * A splitter window, left = hier tree, right = autogen schematics -DONE
-#  * Status bar shows current hierarchy path -DONE
-#  * Window title shows name of module on view -DONE
-#  * Schematic to be displayed is determined by tree selection. -DONE
-#     Alternatively, double-clicking on the bounding box of a schematic's
-#     child module will bring up the schematics of that module, and
-#     update the hier tree selection accordingly. -DONE
-#  * Doubleclicking the Right mouse button will move up the hierarchy -DONE
-#  * Scrolling the mousewheel will zoom in on the cursor.
-#
-# tmp
-# $Id: wxDebuggy.py,v 1.1 2007-08-25 04:14:32 marty Exp $
-#
-# Resizing can act a bit funny...
-#
-
-# 26-AUG-07: Version 0.1
-# Checked in both Linux and Windows land, and everything's OK!
 
 import wx
 import sys
