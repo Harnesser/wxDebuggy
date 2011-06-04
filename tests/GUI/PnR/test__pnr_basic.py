f4c6d83d94cc015016357850103b3e2231f15e0c
Marty <harnesser+marty@gmail.com>
2011-06-04 18:18:30 +0100

    Added base class for PnR tests

--- tests/GUI/PnR/test__pnr_basic.py
+++ tests/GUI/PnR/test__pnr_basic.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import pickle
-import pprint
-import unittest
+import pnr_test_base
+pnr_test_base.set_paths()
 
-# Set path to module under test
-if os.path.exists('../../../GUI/'): # run from this dir
-    sys.path.append('../../../GUI/')
-    sys.path.append('../../../') # for vv.
-else:
-    print "Can't set the correct path"
-    
 import PnR
 
+class PnR_Basic_Operations( pnr_test_base.PnR_Test_Base ):
 
-class PnR_Basic_Operations( unittest.TestCase ):
-    def test_initi(self):
-        self.assertTrue(True)
+    def test__pickle_loader(self):
+        module = self.load_rtl_module_pickle('spider')
+        self.assertEquals(module.name, 'spider')
+        
+    
