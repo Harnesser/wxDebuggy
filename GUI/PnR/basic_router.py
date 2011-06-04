d9c61bd80e206088090150b82fc301319f640cac
Marty <harnesser+marty@gmail.com>
2011-06-04 23:34:23 +0100

    Small tidyup of basic_router

--- GUI/PnR/basic_router.py
+++ GUI/PnR/basic_router.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 from Drawing_Object import Drawing_Object
-import pprint
 
 class Trace_Router:
 
@@ -19,18 +18,8 @@ class Trace_Router:
     def run(self):
         trace_id = 0
         self._determine_glue_points()
-        
-        for connection in self.connections:
 
-            conn1,conn2 = connection
-            #conn1 = conn1.replace('_iport.','')
-            #conn1 = conn1.replace('_oport.','')                                             
-            #conn2 = conn2.replace('_iport.','')
-            #conn2 = conn2.replace('_oport.','')   
-            print "C1:", conn1
-            print "C2:", conn2
-            
-            # Create the flightline
+        for conn1,conn2 in self.connections:
             name = 'conn_%d' %(trace_id)
             drawobj = Drawing_Object( name=name,
                                       parent=None,
@@ -54,6 +43,4 @@ class Trace_Router:
              
             for pin,position in drawing_obj.glue_points.iteritems():
                 self.glue_points[pin] = position
-
-        pprint.pprint(self.glue_points)
 
