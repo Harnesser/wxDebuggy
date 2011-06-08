25b0aa7adad2b80bcce17d3ae6a9d50847e4cec8
Marty <harnesser+marty@gmail.com>
2011-06-08 02:49:50 +0100

    More path changes so nosetests can be run from root. GUI tests/

--- tests/GUI/test_pnr/pnr_test_base.py
+++ tests/GUI/test_pnr/pnr_test_base.py
@@ -4,22 +4,30 @@ import unittest
 import pickle
 import pprint
 
-PICKLE_DIR = '../../module_pickles/'
-
+def find_base():
+    path = os.getcwd()
+    while path != '/':
+        basename = os.path.basename(path)
+        if basename == 'wxDebuggy' or basename == 'workspace':
+            return path
+        else:
+            path = os.path.dirname(path)
+    return None
+    
 def set_paths():
     # Set path to module under test
-    if os.path.exists('../../../GUI/'): # run from this dir
-        sys.path.append('../../../GUI/')
-        sys.path.append('../../../') # for vv.
-    else:
-        print "Can't set the correct path"
-        
+    proj_dir = find_base()
+    sys.path.append(proj_dir) # for vv.
+    sys.path.append( os.path.join( proj_dir, 'GUI') )
+
         
 class PnR_Test_Base( unittest.TestCase ):
     
     def load_rtl_module_pickle( self, module_name ):
         """ Load pickled RTL module datastructure. """
-        hPICKLE = open(PICKLE_DIR + module_name +'.dat','rb')
+        pickle_dir = os.path.join( find_base(), 'tests', 'module_pickles' )
+        pickle_name = os.path.join( pickle_dir, module_name +'.dat' )
+        hPICKLE = open( pickle_name,'rb')
         module = pickle.load(hPICKLE)
         pprint.pprint(module)
         
