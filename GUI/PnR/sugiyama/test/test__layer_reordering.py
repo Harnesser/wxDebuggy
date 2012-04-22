e74dd0ec55038405dca6850eab3981345fe7e5f4
Marty <harnesser+marty@gmail.com>
2012-04-22 22:32:43 +0100

    more changes

--- GUI/PnR/sugiyama/test/test__layer_reordering.py
+++ GUI/PnR/sugiyama/test/test__layer_reordering.py
@@ -8,21 +8,32 @@ from .. import reordering
 DEBUG = False
 
 class Test_SugiyamaLayerReordering():
-       
-	def test__reorder_gates1_phase1(self):
-		G = helpers.paper_cct()
-		G.update()
-		print G.display()
+ 
+    """      
+    def test__reorder_gd09(self):
+        G = helpers.paper_cct()
+        G.update()
+        print G.display()
 
-		eng = reordering.Reordering_Engine()
-		eng.set_graph(G)
-		#self.show_conn_matrices(eng.G, 'I N I T I A L')
-		#        
-		assert eng.G.count_crossovers() == 8
-		#for x in eng.gen_phase1():
-		#    self.show_conn_matrices(eng.G)
-		#        
-		assert eng.G.count_crossovers() == 0
-		#
-		#self.show_conn_matrices(eng.G, 'F I N A L   R E S U L T')
+        eng = reordering.Reordering_Engine()
+        eng.set_graph(G)
+        assert eng.G.count_crossovers() == 28
 
+        eng.run(debug=True)    
+        assert eng.G.count_crossovers() == 0
+"""     
+
+    def test__reorder_sugiyama_cct(self):
+        G = helpers.orig_sugiyama_cct()
+        G.update()
+        print G.display()
+        
+        eng = reordering.Reordering_Engine()
+        eng.set_graph(G)
+        assert eng.G.count_crossovers() == 8
+        
+        eng.run(debug=True)
+        print G.display()
+        assert eng.G.count_crossovers() == 1
+        
+        
