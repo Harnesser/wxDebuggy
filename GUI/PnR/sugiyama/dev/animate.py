3ae6d183e5f9fcf03873358b480ed62f976bff45
Marty <harnesser+marty@gmail.com>
2012-04-22 22:35:40 +0100

    Animation working. Algorithm isn't

--- GUI/PnR/sugiyama/dev/animate.py
+++ GUI/PnR/sugiyama/dev/animate.py
@@ -25,23 +25,25 @@ class MainWindow(wx.Frame):
         self.vertices = []
         self.edges = []
         
-        self.timer = wx.Timer(self)
-        self.timer.Start(1000) # 1000 milliseconds = 1 second
-
-        self.Bind(wx.EVT_TIMER, self.OnTimer)
         self.Bind(wx.EVT_LEFT_UP, self.OnClick)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         
         self.layout_iter = None
         self.G1 = None
+        self.layer = 10909090909090
+        self.direction = ''
+        self.eng = reordering.Reordering_Engine()
         self.port_positions = {}
         self.init_graph()
         
         
     def init_graph(self):
-        self.G1 = ccts.orig_sugiyama_cct()
+        self.G1 = ccts.orig_sugiyama_single_port_cct()
         self.G1.update()
-        
+        self.eng.set_graph(self.G1)
+        self.layout_iter = self.eng.gen_run()  # plug in a generator here
+        self.update_positions()
+
         
     def OnPaint(self, e):
         #print "OnPaint called"
@@ -50,8 +52,8 @@ class MainWindow(wx.Frame):
 
         # Go through the list of vertices to draw all of them
         for vertex in self.vertices:
-            x,y,v = vertex
-            self.draw_vertex(dc, v, x, y)
+            x,y,v,c = vertex
+            self.draw_vertex(dc, v, x, y, c)
 
         # draw the connetions
         if not self.port_positions:
@@ -63,11 +65,13 @@ class MainWindow(wx.Frame):
                 x2,y2 = self.port_positions[ (edge.target, edge.target_port) ]
                 dc.DrawLine(x1,y1,x2,y2)
 
+        self.display_crossovers(dc)
+        
 
-    def draw_vertex(self, dc, vertex, x, y):
+    def draw_vertex(self, dc, vertex, x, y, colour):
         """ """
         # module block
-        dc.SetBrush(wx.Brush(wx.BLUE))
+        dc.SetBrush(wx.Brush(colour))
         dc.DrawRectangle( x, y, MODULE_WIDTH, MODULE_HEIGHT)
         
         # instance name
@@ -107,30 +111,42 @@ class MainWindow(wx.Frame):
             self.port_positions[ (vertex.get_name(), port.get_name()) ] = (xp+2, yp+2)
             xp += PORT_WIDTH + 5                                   
         
+    def display_crossovers(self,dc):
+        dc.SetTextForeground(wx.BLACK)
+        font = wx.Font( pointSize=20, family=wx.MODERN,
+            style=wx.NORMAL, weight=wx.BOLD )
+        dc.SetFont(font)
+        dc.DrawText( 'Crossovers: %d' % (self.G1.count_crossovers()) , 50, 600 )
     
-    def OnTimer(self, e):
-        #print "OnTimer called"
-        self.vertices = []
         
-        # Get next graph layout
-#        try:
-#            layout = self.layout_iter.next()
-#        except StopIteration:
-#            self.timer.Stop()
-#            print "Done"
-#            return
+    def next(self):
+        """ Take the next step in the layout algorithm. """
+        try:
+            (self.layer, self.direction) = self.layout_iter.next()
+            print "Animate: layer %d, %s" % (self.layer, self.direction)
+        except StopIteration:
+            print "Out of stuff..."
+            pass
+        
             
+    def update_positions(self):
+        self.vertices = []
         for i in range( self.G1.count_layers() ):
+            if i == self.layer:
+                colour = wx.RED
+            else:
+                colour = wx.BLUE
             y_pos = 40 + ( i * (MODULE_HEIGHT * 2.0) )
             for j in range( len(self.G1.vertices[i]) ):
                 vertex = self.G1.vertices[i][j]
                 x_pos = 75 + ( j * (MODULE_WIDTH * 1.5) )
-                self.vertices.append( [x_pos, y_pos, vertex])
-                
-        self.Refresh()
+                self.vertices.append( [x_pos, y_pos, vertex, colour])
+
 
     def OnClick(self, e):
         """ Advance the layout alg?. """
+        self.next()
+        self.update_positions()
         self.Refresh()
         
 def main():
